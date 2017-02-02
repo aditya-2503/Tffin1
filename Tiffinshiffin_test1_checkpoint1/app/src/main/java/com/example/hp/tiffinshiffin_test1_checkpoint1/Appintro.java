2fee280c4279880ed1c5d78b4f5e43c1f7069634
aditya-2503 <adityaamit25@gmail.com>
2017-02-02 18:38:24 +0530

    code

---
+++ Tiffinshiffin_test1_checkpoint1/app/src/main/java/com/example/hp/tiffinshiffin_test1_checkpoint1/Appintro.java
@@ -0,0 +1,15 @@
+package com.example.hp.tiffinshiffin_test1_checkpoint1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by HP on 1/31/2017.
+ */
+
+public class Appintro extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
